25153d261ab0fc5745d1997f9b62dbc89201482a
Tricrepe <644568681@qq.com>
2016-03-01 22:06:32 +0800

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 gulp.task('sass',function(){
-  gulp.src('src/main.scss').pipe(sass()).pipe(gulp.dest('dist/'))
-})
+  gulp.src('src/main.scss').pipe(sass()).pipe(autoprefixer()).pipe(gulp.dest('dist/'))
+});
+gulp.task('default',['sass'])
