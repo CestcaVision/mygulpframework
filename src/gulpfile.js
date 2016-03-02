3817d2b28be098df2b052eabc04897d20c9389a1
Tricrepe <644568681@qq.com>
2016-03-02 11:01:03 +0800

    add gulp-watch

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -6,15 +6,22 @@ var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 
 gulp.task('minifyimage', function () {
-	  gulp.src('src/images/*')
+	  gulp.src('assets/*')
 		.pipe(imagemin({
 			progressive: true,
 			svgoPlugins: [{removeViewBox: false}],
 			use: [pngquant()]
 		}))
-		.pipe(gulp.dest('dist/images'));
+		.pipe(gulp.dest('../assets'));
+});
+gulp.task('cp',function(){
+	gulp.src("index.html").pipe(gulp.dest(".."))
 });
 gulp.task('sass',function(){
-  gulp.src('src/style/main.scss').pipe(sass()).pipe(autoprefixer()).pipe(minify()).pipe(gulp.dest('dist/'))
+  gulp.src('styles/main.scss').pipe(sass()).pipe(autoprefixer()).pipe(minify()).pipe(gulp.dest('../styles'))
 });
-gulp.task('default',['sass','minifyimage'])
+gulp.task('watch',function(){
+	gulp.watch(['*.html'],['cp']);
+	gulp.watch(['styles/*.scss'],['sass']);
+});
+gulp.task('default',['minifyimage','watch'])
